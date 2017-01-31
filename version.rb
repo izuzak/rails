8bc51b394cddaeaaffa4a5b9a4e2201ea5c164bb
Ivan Žužak <ivan.zuzak@github.com>
2017-01-31 15:39:13 +0100

    test

--- version.rb
+++ version.rb
@@ -1,5 +1,5 @@
 module Rails
-  # Returns the version of the currently loaded Rails as a <tt>Gem::Version</tt>
+  # Returns the version of the currently loaded Rails as a <tt>Gem::Version</tt> test
   def self.gem_version
     Gem::Version.new VERSION::STRING
   end
